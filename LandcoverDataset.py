1ae2fd4a4cb69da52d35e215c2df8e4f0611a476
Alex <8147936-phlaster@users.noreply.gitlab.com>
2024-08-12 15:32:30 +0300

    simplifying `__init__` method, faster loadtimes, when load dataset partially

--- LandcoverDataset.py
+++ LandcoverDataset.py
@@ -35,18 +35,26 @@ CLASS_NAMES = [
 
 
 class LandcoverDataset(Dataset):
-    def __init__(self, img_path, mask_path, batch_size, n_random=None, transforms=None, indexes=None, n_classes=5):
+    def __init__(self, img_path, mask_path, batch_size, n_random=None, transforms=None, indexes=None, n_classes=5, images_dimensions=(512, 512, 10)):
+        self.images_dimensions = images_dimensions
         self.num_classes = n_classes
         self.img_path = img_path
         self.mask_path = mask_path
         self.file_names = [f for f in os.listdir(img_path) if f.endswith('.tif') ]
-        if n_random:
-            self.file_names = sample(self._filter_by_size(self.file_names, img_path, (512, 512)), n_random)
-        else:
-            self.file_names = self._filter_by_size(self.file_names, img_path, (512, 512))
+        self.spectral_indices = [SpectralIndex(index) for index in indexes if indexes is not None]
+        self.loader = DataLoader(self, batch_size=batch_size, num_workers=os.cpu_count())
+
+        self.file_names = self._filter_by_size(
+            filenames=np.random.permutation(self.file_names),
+            img_path=img_path,
+            dims=images_dimensions,
+            n_max=n_random
+        ) if n_random else self._filter_by_size(
+            filenames=self.file_names,
+            img_path=img_path,
+            dims=images_dimensions
+        )
         
-        self.indexes = indexes if indexes is not None else []
-        self.spectral_indices = [SpectralIndex(index) for index in self.indexes]
         self.transforms = Compose([
             RandomVerticalFlip(p=0.5),
             RandomHorizontalFlip(p=0.5),
@@ -54,14 +62,9 @@ class LandcoverDataset(Dataset):
             ChannelDropout(channel_drop_range=(1, 2), fill_value=0, p=0.3) #, protect_last=len(self.indexes))
         ]) if transforms is None else transforms
         
-        with multiprocessing.Pool(processes=os.cpu_count()) as pool: # otherwise too slow
-            self.images = list(tqdm(
-                pool.imap(self._load_and_preprocess_image, self.file_names),
-                total=len(self.file_names),
-                desc='Loading and preprocessing images'))
-        self.masks = [self._load_and_preprocess_mask(f) for f in tqdm(self.file_names, desc='Loading and preprocessing masks')]
-        self.loader = DataLoader(self, batch_size=batch_size, num_workers=os.cpu_count())
-
+        self.images = self._preprocess_images()
+        self.masks = self._preprocess_masks()
+        
     def __len__(self):
         return len(self.file_names)
 
@@ -72,22 +75,32 @@ class LandcoverDataset(Dataset):
             image = self.transforms(image)
         class_counts = torch.bincount(torch.tensor(mask).view(-1), minlength=self.num_classes)        
         return image.clone().detach(), torch.tensor(mask, dtype=torch.long), class_counts
-    
 
-    def _filter_by_size(self, file_names, img_path, target_size):
+    def _normalize(self, image):
+        image = image.astype(np.float32)
+        min_val = np.min(image, axis=(0, 1), keepdims=True)
+        max_val = np.max(image, axis=(0, 1), keepdims=True)
+        normalized = (image - min_val) / (max_val - min_val + 1e-8)
+        return normalized
+
+    def _filter_by_size(self, filenames, img_path, dims, n_max=0):
+        assert 0<n_max, f"n_max can't be negative"
+        if not n_max:
+            n_max = len(filenames)
         filtered_files = []
-        for filename in tqdm(file_names, desc='Filtering files with wrong dimensions:'):
+        for i in tqdm(range(n_max), desc='Filtering out files with wrong dimensions:', leave=False):
+            filename = filenames[i]
             try:
                 with tifffile.TiffFile(os.path.join(img_path, filename)) as tif:
-                    if tif.pages[0].shape[:2] == target_size:
-                        filtered_files.append(filename)
+                    if tif.pages[0].shape == dims:
+                        filtered_files.append(filename)      
+
             except Exception as e:
                 print(f"Error reading {filename}: {e}")
                 continue
         return filtered_files
 
-        
-    def _load_and_preprocess_image(self, filename):
+    def _one_image(self, filename):
         image = tifffile.imread(os.path.join(self.img_path, filename))
         image = self._normalize(image)
         image = np.moveaxis(image, -1, 0)  # Convert (H, W, C) to (C, H, W) for PyTorch
@@ -97,21 +110,27 @@ class LandcoverDataset(Dataset):
             index_band = si.apply(image + noise)
             index_band = self._normalize(index_band[np.newaxis, ...])  # Add channel dimension and normalize
             image = np.concatenate((image, index_band), axis=0)
-        
         return image
 
-    def _load_and_preprocess_mask(self, filename):
-        mask = tifffile.imread(os.path.join(self.mask_path, filename))
-        return mask
+    def _preprocess_images(self):
+        with multiprocessing.Pool(processes=os.cpu_count()) as pool: # otherwise too slow
+            images = list(tqdm(
+                pool.imap(self._one_image, self.file_names),
+                desc='Loading images',
+                leave=False))
+        return images
 
-    def _normalize(self, image):
-        image = image.astype(np.float32)
-        min_val = np.min(image, axis=(0, 1), keepdims=True)
-        max_val = np.max(image, axis=(0, 1), keepdims=True)
-        normalized = (image - min_val) / (max_val - min_val + 1e-8)
-        return normalized
+    def _preprocess_masks(self):
+        masks = [
+            tifffile.imread(os.path.join(self.mask_path, f))
+            for f in tqdm(self.file_names, desc='Loading masks', leave=False)
+        ]
+        return masks
 
-    
+    def set_batchsize(self, new_batchsize):
+        assert new_batchsize > 0, "Enter positive batch size"
+        self.loader = DataLoader(self, batch_size=new_batchsize, num_workers=os.cpu_count())
+
     def getinfo(self):
         num_images = len(self.images)
         img_shape = self.images[0].shape if num_images > 0 else "N/A"
@@ -126,7 +145,7 @@ class LandcoverDataset(Dataset):
         print(f"Image Data Type: {img_dtype}")
         print(f"Mask Data Type: {mask_dtype}")
         print(f"Transformations: {self.transforms}")
-        print(f"Spectral Indices: {self.indexes}")
+        print(f"Spectral Indices: \n\t{"\n\t".join([si.formula for si in self.spectral_indices])}")
 
     def plot_sample(self, n, r=2, g=1, b=0, index=""):
         def apply_palette(mask, palette):
@@ -222,7 +241,6 @@ class LandcoverDataset(Dataset):
         plt.subplots_adjust(wspace=0.1, hspace=0.1)
         plt.tight_layout()
         plt.show()
-        
         
     def rand_samp_names(self):
         name = choice(self.file_names)
